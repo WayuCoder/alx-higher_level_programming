2c142909a8077cc9309d17a4c004c234236a7d56
WayuCoder <wayuberry@gmail.com>
2022-06-20 02:47:56 -0700

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
-    li = 0
-    for i in range(0, x):
-        try:
-            print(my_list[i], end="")
-            li += 1
-        except:
-            break
+    i = 0
+    try:
+        while i is not x:
+            print(my_list[i], end='')
+            i += 1
+    except IndexError:
+        None
     print()
-    return (li)
+    return i
